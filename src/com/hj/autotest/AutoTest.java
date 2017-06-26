ef9da157bd841f1b0d363d37218bddfb3593d8c9
zhenzhen15 <janyzhen15@163.com>
2017-06-26 12:03:47 +0800

    首次提交到本地仓库

---
+++ src/com/hj/autotest/AutoTest.java
@@ -0,0 +1,15 @@
+package com.hj.autotest;
+
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+public class AutoTest extends UiAutomatorTestCase {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         new UiAutomatorTool("quanshi1","com.hj.autotest.QuanShi" ,"testDemo","1");
+	}
+
+}
