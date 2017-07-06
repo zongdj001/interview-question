e7d1e998b6a1a03367c516a0b07e79ea5fec8e1a
zhenzhen15 <janyzhen15@163.com>
2017-07-06 19:40:51 +0800

    zhenzhen update uiautomatorassistant.java

---
+++ bin/classes.dex.d
@@ -0,0 +1,6 @@
+D:\jenny\Sample_Uia\bin\classes.dex : \
+D:\jenny\Sample_Uia\bin\classes\com\hj\autotest\AutoTest.class \
+D:\jenny\Sample_Uia\bin\classes\com\hj\autotest\QuanShi.class \
+D:\jenny\Sample_Uia\bin\classes\com\hj\autotest\UiautomatorAsssistant.class \
+D:\jenny\Sample_Uia\bin\classes\com\hj\autotest\UiAutomatorTool.class \
+D:\jenny\Sample_Uia\bin\classes\quanshipacage\QuanShi.class \
